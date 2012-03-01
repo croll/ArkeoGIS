6ffeb305c116aa2c28ac194f8b97f11fe69cee73
Nicolas Dimitrijevic <github-nicospam@sengena.com>
2012-03-01 16:44:19 +0100

    add languages files

---
+++ mod/arkeogis/lang/de_DE.js
@@ -0,0 +1,21 @@
+{
+  "Requ\u00eates archiv\u00e9es" : "Requ\u00eates archiv\u00e9es",
+  "Caract\u00e9risation :" : "Caract\u00e9risation :",
+  "Sites exceptionnels" : "Sites exceptionnels",
+  "Afficher<br\/>la carte" : "Afficher<br\/>la carte",
+  "Afficher<br\/>le tableur" : "Afficher<br\/>le tableur",
+  "R\u00e9initialiser la requ\u00eate" : "R\u00e9initialiser la requ\u00eate",
+  "Imprimer" : "Imprimer",
+  "Exporter" : "Exporter",
+  "Import d'une base" : "Import d'une base",
+  "S\u00e9parateur" : "S\u00e9parateur",
+  "Utilisez \\\\t pour une tabulation" : "Utilisez \\\\t pour une tabulation",
+  "Lignes \u00e0 ignorer" : "Lignes \u00e0 ignorer",
+  "Nombre de ligne en d\u00e9but de fichier \u00e0 ne pas traiter" : "Nombre de ligne en d\u00e9but de fichier \u00e0 ne pas traiter",
+  "Caract\u00e8re d'\u00e9chapement" : "Caract\u00e8re d'\u00e9chapement",
+  "Langue utilis\u00e9e pour caract\u00e9riser les p\u00e9riodes\/mobilier\/immobilier" : "Langue utilis\u00e9e pour caract\u00e9riser les p\u00e9riodes\/mobilier\/immobilier",
+  "Effectuer un autre import" : "Effectuer un autre import",
+  "Selection" : "Selection",
+  "ALL" : "ALL",
+  "NONE" : "NONE"
+}
