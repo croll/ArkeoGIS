9425b6d5ff8b561b1a399568fca6bd33feabb94c
Nicolas Dimitrijevic <niclone@croll.fr>
2012-03-09 14:51:07 +0100

    limit to period_isrange=1

--- mod/arkeogis/ArkeoGIS.php
+++ mod/arkeogis/ArkeoGIS.php
@@ -66,12 +66,11 @@ class ArkeoGIS {
 
 
 	public static function search_sites($search, $columns, $addtable=array()) {
-		$addtable=array('ark_site_period' => isset($addtable['ark_site_period']) ? $addtable['ark_site_period'] : false,
-										'ark_siteperiod_production' => isset($addtable['ark_siteperiod_production']) ? $addtable['ark_siteperiod_production'] : false,
+		$addtable=array('ark_siteperiod_production' => isset($addtable['ark_siteperiod_production']) ? $addtable['ark_siteperiod_production'] : false,
 										'ark_siteperiod_furniture' => isset($addtable['ark_siteperiod_furniture']) ? $addtable['ark_siteperiod_furniture'] : false,
 										'ark_siteperiod_realestate' => isset($addtable['ark_siteperiod_realestate']) ? $addtable['ark_siteperiod_realestate'] : false);
 
-		$query=' WHERE (1=1) ';
+		$query=' WHERE sp_period_isrange=1 ';
 		$args=array();
 
 		if (isset($search['db_include']) && count($search['db_include'])) {
@@ -85,7 +84,6 @@ class ArkeoGIS {
 		}
 
 		if (isset($search['period_include']) && count($search['period_include'])) {
-			$addtable['ark_site_period']=true;
       $query.=' AND (0=1 ';
 			foreach($search['period_include'] as $period) {
 				$query.=' OR sp_period_start >= ? AND sp_period_start <= ?';
@@ -96,7 +94,6 @@ class ArkeoGIS {
 		}
 
 		if (isset($search['period_exclude']) && count($search['period_exclude'])) {
-			$addtable['ark_site_period']=true;
       $query.=' AND (0=1 ';
 			foreach($search['period_exclude'] as $period) {
 				$query.=' OR NOT (sp_period_start >= ? AND sp_period_start <= ?)';
@@ -112,7 +109,6 @@ class ArkeoGIS {
 		}
 
 		if (isset($search['knowledge_include']) && count($search['knowledge_include'])) {
-			$addtable['ark_site_period']=true;
 			$query.=' AND sp_knowledge_type IN(?)';
 			$args[]=$search['knowledge_include'];
 		}
@@ -123,61 +119,52 @@ class ArkeoGIS {
 		}
 
 		if (isset($search['production_include']) && count($search['production_include'])) {
-			$addtable['ark_site_period']=true;
 			$addtable['ark_siteperiod_production']=true;
 			$query.=' AND sp_production_id IN (?)';
 			$args[]=$search['production_include'];
 		}
 
 		if (isset($search['production_exclude']) && count($search['production_exclude'])) {
-			$addtable['ark_site_period']=true;
 			$addtable['ark_siteperiod_production']=true;
 			$query.=' AND sp_production_id NOT IN (?)';
 			$args[]=$search['production_exclude'];
 		}
 
 		if (isset($search['production_exceptional']) && $search['production_exceptional'] == 1) {
-			$addtable['ark_site_period']=true;
 			$addtable['ark_siteperiod_production']=true;
 			$query.=' AND sp_exceptional = 1';
 		}
 
 		if (isset($search['furniture_include']) && count($search['furniture_include'])) {
-			$addtable['ark_site_period']=true;
 			$addtable['ark_siteperiod_furniture']=true;
 			$query.=' AND sf_furniture_id IN (?)';
 			$args[]=$search['furniture_include'];
 		}
 
 		if (isset($search['furniture_exclude']) && count($search['furniture_exclude'])) {
-			$addtable['ark_site_period']=true;
 			$addtable['ark_siteperiod_furniture']=true;
 			$query.=' AND sf_furniture_id NOT IN (?)';
 			$args[]=$search['furniture_exclude'];
 		}
 
 		if (isset($search['furniture_exceptional']) && $search['furniture_exceptional'] == 1) {
-			$addtable['ark_site_period']=true;
 			$addtable['ark_siteperiod_furniture']=true;
 			$query.=' AND sf_exceptional = 1';
 		}
 
 		if (isset($search['realestate_include']) && count($search['realestate_include'])) {
-			$addtable['ark_site_period']=true;
 			$addtable['ark_siteperiod_realestate']=true;
 			$query.=' AND sr_realestate_id IN (?)';
 			$args[]=$search['realestate_include'];
 		}
 
 		if (isset($search['realestate_exclude']) && count($search['realestate_exclude'])) {
-			$addtable['ark_site_period']=true;
 			$addtable['ark_siteperiod_realestate']=true;
 			$query.=' AND sr_realestate_id NOT IN (?)';
 			$args[]=$search['realestate_exclude'];
 		}
 
 		if (isset($search['realestate_exceptional']) && $search['realestate_exceptional'] == 1) {
-			$addtable['ark_site_period']=true;
 			$addtable['ark_siteperiod_realestate']=true;
 			$query.=' AND sr_exceptional = 1';
 		}
@@ -185,9 +172,7 @@ class ArkeoGIS {
 
 		$select="SELECT $columns";
 		$select.=" FROM ark_site";
-		if ($addtable['ark_site_period']) {
-			$select.=" LEFT JOIN ark_site_period ON sp_site_code = si_code";
-		}
+		$select.=" LEFT JOIN ark_site_period ON sp_site_code = si_code";
 		if ($addtable['ark_siteperiod_production']) {
 			$select.=" LEFT JOIN ark_siteperiod_production ON sp_site_period_id = ark_site_period.sp_id";
 		}
