5ba063a1d19351057ea185c0e25b74c738318413
Nicolas Dimitrijevic <github-nicospam@sengena.com>
2012-03-29 00:29:18 +0200

    export should now be ok

--- mod/arkeogis/Main.php
+++ mod/arkeogis/Main.php
@@ -230,10 +230,10 @@ class Main {
 
 		$q=json_decode($_REQUEST['q'], true);
 		
-		$columns="si_code, si_name, si_description, si_city_id, ST_AsGeoJSON(si_geom) as coords, si_centroid, si_occupation, si_creation, si_modification"; // ark_site
+		$columns="ark_site.si_id, si_code, si_name, si_description, si_city_id, ST_AsGeoJSON(si_geom) as coords, si_centroid, si_occupation, si_creation, si_modification"; // ark_site
 		$columns.=", ci_code, ci_name, ci_country, ci_geom"; // ark_city
 		$columns.=", da_name, da_description, da_creation, da_modification"; // ark_database
-    $columns.=", sp_knowledge_type, sp_comment, sp_bibliography"; // ark_site_period
+    $columns.=", ark_site_period.sp_id, sp_knowledge_type, sp_comment, sp_bibliography"; // ark_site_period
     $columns.=", sr_exceptional, sf_exceptional, sp_exceptional"; // exceptionals
 		$columns.=", (SELECT node_path FROM ark_period WHERE pe_id=sp_period_start) AS period_start";
 		$columns.=", (SELECT node_path FROM ark_period WHERE pe_id=sp_period_end) AS period_end";
@@ -249,9 +249,11 @@ class Main {
                                   'ark_database' => true
                                 ),
 																false, // limit
-                                '', // group_by
+                                '', // group_by,
+                                '',
 																'ark_site.si_code, ark_site.si_id', // orderby
-                                false // getcount
+                                false, // getcount
+                                false  // onlysprange
     );
 		
 		$strings=ArkeoGIS::load_strings();
@@ -264,7 +266,11 @@ class Main {
     $fp = fopen('php://output', 'w');
     fputcsv($fp, array('SITE_ID_SOURCE', 'BASE_SOURCE', 'NOM_SITE', 'NOM_COMMUNE_PRINCIPALE', 'CODE_COMMUNE', 'SYSTEME_PROJECTION', 'LONGITUDE_X ', 'LATITUDE_Y', 'LONGITUDE_X_BIS', 'LATITUDE_Y_BIS', 'ALTITUDE Z', 'CENTRE_COMMUNE', 'ETAT_CONNAISSANCES', 'OCCUPATION', 'DATATION_DEBUT_PLUS_FINE', 'DATATION_FIN_PLUS_FINE', 'IMMO_NIV1', 'IMMO_NIV2', 'IMMO_NIV3', 'IMMO_NIV4', 'PROFONDEUR_VESTIGES', 'IMMO_EXP', 'MOB_NIV1', 'MOB_NIV2', 'MOB_NIV3', 'MOB_NIV4', 'MOB_EXP', 'PROD_NIV1', 'PROD_NIV2', 'PROD_NIV3', 'PROD_EXP', 'BIBLIOGRAPHIE', 'REMARQUES'), ";", '"');
 		
+    $latest_siid=-1;
+    $latest_spid=-1;
 		foreach($res['sites'] as $row) {
+      $newsiid=($latest_siid != $row['si_id']);
+      $newspid=($latest_spid != $row['sp_id']);
       $coords=json_decode($row['coords']);
       $period_start=ArkeoGIS::node_path_to_array($row['period_start'], $strings['period']);
       $period_end=ArkeoGIS::node_path_to_array($row['period_end'], $strings['period']);
@@ -272,40 +278,42 @@ class Main {
       $furniture=ArkeoGIS::node_path_to_array($row['furniture'], $strings['furniture']);
       $production=ArkeoGIS::node_path_to_array($row['production'], $strings['production']);
       fputcsv($fp, array(
-                $row['si_code'],                 // SITE_ID_SOURCE
-                $row['da_name'],                 // BASE_SOURCE
-                $row['si_name'],                 // NOM_SITE
-                $row['ci_name'],                 // NOM_COMMUNE_PRINCIPALE
-                $row['ci_code'],                 // CODE_COMMUNE
-                'WGS84',                         // SYSTEME_PROJECTION
-                $coords->coordinates[0],         // LONGITUDE_X
-                $coords->coordinates[1],         // LATITUDE_Y
-                '',                              // LONGITUDE_X_BIS
-                '',                              // LATITUDE_Y_BIS
-                $coords->coordinates[2] == -999 ? '' : $coords->coordinates[2] ,       // ALTITUDE Z
-                $row['si_centroid'],             // CENTRE_COMMUNE
-                $strings['knowledge'][$row['sp_knowledge_type']],  // ETAT_CONNAISSANCES
-                $strings['occupation'][$row['si_occupation']],     // OCCUPATION
-                count($period_start) ? $period_start[count($period_start)-1] : '', // DATATION_DEBUT_PLUS_FINE
-                count($period_end) ? $period_end[count($period_end)-1] : '',       // DATATION_FIN_PLUS_FINE
-                isset($realestate[0]) ? $realestate[0] : '',       // IMMO_NIV1
-                isset($realestate[1]) ? $realestate[1] : '',       // IMMO_NIV2
-                isset($realestate[2]) ? $realestate[2] : '',       // IMMO_NIV3
-                isset($realestate[3]) ? $realestate[3] : '',       // IMMO_NIV4
-                '', // PROFONDEUR_VESTIGES
-                $row['sr_exceptional'], // IMMO_EXP
-                isset($furniture[0]) ? $furniture[0] : '',         // MOB_NIV1
-                isset($furniture[1]) ? $furniture[1] : '',         // MOB_NIV2
-                isset($furniture[2]) ? $furniture[2] : '',         // MOB_NIV3
-                isset($furniture[3]) ? $furniture[3] : '',         // MOB_NIV4
-                $row['sf_exceptional'], // MOB_EXP
-                isset($production[0]) ? $production[0] : '',       // PROD_NIV1
-                isset($production[1]) ? $production[1] : '',       // PROD_NIV2
-                isset($production[2]) ? $production[2] : '',       // PROD_NIV3
-                $row['sp_exceptional'],                            // PROD_EXP
-                $row['sp_bibliography'],                           // BIBLIOGRAPHIE
-                $row['sp_comment']                                 // REMARQUES
+                $row['si_code'],                                                                       // SITE_ID_SOURCE
+                $newsiid ? $row['da_name'] : '',                                                       // BASE_SOURCE
+                $newsiid ? $row['si_name'] : '',                                                       // NOM_SITE
+                $newsiid ? $row['ci_name'] : '',                                                       // NOM_COMMUNE_PRINCIPALE
+                $newsiid ? $row['ci_code'] : '',                                                       // CODE_COMMUNE
+                $newsiid ? 'WGS84' : '',                                                               // SYSTEME_PROJECTION
+                $newsiid ? $coords->coordinates[0] : '',                                               // LONGITUDE_X
+                $newsiid ? $coords->coordinates[1] : '',                                               // LATITUDE_Y
+                '',                                                                                    // LONGITUDE_X_BIS
+                '',                                                                                    // LATITUDE_Y_BIS
+                $newsiid ? ($coords->coordinates[2] == -999 ? '' : $coords->coordinates[2]) : '',      // ALTITUDE Z
+                $newsiid ? $row['si_centroid'] : '',                                                   // CENTRE_COMMUNE
+                $newsiid && $row['sp_knowledge_type'] ? $strings['knowledge'][$row['sp_knowledge_type']] : '', // ETAT_CONNAISSANCES
+                $newsiid ? $strings['occupation'][$row['si_occupation']] : '',                         // OCCUPATION
+                $newspid ? (count($period_start) ? $period_start[count($period_start)-1] : '') : '',   // DATATION_DEBUT_PLUS_FINE
+                $newspid ? (count($period_end) ? $period_end[count($period_end)-1] : '') : '',         // DATATION_FIN_PLUS_FINE
+                isset($realestate[0]) ? $realestate[0] : '',                                           // IMMO_NIV1
+                isset($realestate[1]) ? $realestate[1] : '',                                           // IMMO_NIV2
+                isset($realestate[2]) ? $realestate[2] : '',                                           // IMMO_NIV3
+                isset($realestate[3]) ? $realestate[3] : '',                                           // IMMO_NIV4
+                '',                                                                                    // PROFONDEUR_VESTIGES
+                $row['sr_exceptional'],                                                                // IMMO_EXP
+                isset($furniture[0]) ? $furniture[0] : '',                                             // MOB_NIV1
+                isset($furniture[1]) ? $furniture[1] : '',                                             // MOB_NIV2
+                isset($furniture[2]) ? $furniture[2] : '',                                             // MOB_NIV3
+                isset($furniture[3]) ? $furniture[3] : '',                                             // MOB_NIV4
+                $row['sf_exceptional'],                                                                // MOB_EXP
+                isset($production[0]) ? $production[0] : '',                                           // PROD_NIV1
+                isset($production[1]) ? $production[1] : '',                                           // PROD_NIV2
+                isset($production[2]) ? $production[2] : '',                                           // PROD_NIV3
+                $newspid ? $row['sp_exceptional'] : '',                                                // PROD_EXP
+                $newspid ? $row['sp_bibliography'] : '',                                               // BIBLIOGRAPHIE
+                $newspid ? $row['sp_comment'] : ''                                                     // REMARQUES
               ), ";", '"');
+      $latest_siid=$row['si_id'];
+      $latest_spid=$row['sp_id'];
 		}
 		    
   }
