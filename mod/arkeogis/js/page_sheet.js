2ecbf0173aa2b3049e5089a7afbac1ace40c6648
Nicolas Dimitrijevic <github-nicospam@sengena.com>
2012-03-29 12:30:14 +0200

    fixed print

--- mod/arkeogis/js/page_sheet.js
+++ mod/arkeogis/js/page_sheet.js
@@ -2,7 +2,7 @@ window.addEvent('domready', function() {
     new Request.JSON({
 	'url': '/ajax/call/arkeogis/showthesheet',
 	'onSuccess': function(res) {
-	    display_sheet(res);
+	    display_sheet(res.sites);
 	    window.print();
 	}
     }).post(decodeURI(new URI().get('fragment')));
