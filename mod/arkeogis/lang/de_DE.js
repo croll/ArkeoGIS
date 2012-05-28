99c1c54d349917263f127b964da44860f9ffc02b
Nicolas Dimitrijevic <niclone@croll.fr>
2012-05-28 17:40:26 +0200

    lang update

--- mod/arkeogis/lang/de_DE.js
+++ mod/arkeogis/lang/de_DE.js
@@ -113,5 +113,12 @@
   "Aucun r\u00e9sultat" : "Kein ergebnis",
   "(exceptionals only)" : "(Nur aussergew\u00f6hnliche)",
   "Error" : "Fehler",
-  "Documents" : "Unterlagen"
+  "Documents" : "Unterlagen",
+  "Vous pouvez pr\u00e9senter en quelques lignes votre base de donn\u00e9e." : "Vous pouvez pr\u00e9senter en quelques lignes votre base de donn\u00e9e.",
+  "Langue utilis\u00e9e pour caract\u00e9riser les p\u00e9riodes\/mobilier\/immobilier" : "Langue utilis\u00e9e pour caract\u00e9riser les p\u00e9riodes\/mobilier\/immobilier",
+  "Vous devez choisir au moins une base, une p\u00e9riode et une caract\u00e9risation" : "Vous devez choisir au moins une base, une p\u00e9riode et une caract\u00e9risation",
+  "Seulement %d sites seront affich\u00e9 sur %d au total. Souhaitez-vous t\u00e9l\u00e9charger la liste au format csv ? Cliquer sur le bouton Cancel affichera les 1000 premiers sites." : "Seulement %d sites seront affich\u00e9 sur %d au total. Souhaitez-vous t\u00e9l\u00e9charger la liste au format csv ? Cliquer sur le bouton Cancel affichera les 1000 premiers sites.",
+  "Seulement %d sites seront affich\u00e9 sur %d au total. Souhaitez-vous plut\u00f4t t\u00e9l\u00e9charger la liste au format csv ?" : "Seulement %d sites seront affich\u00e9 sur %d au total. Souhaitez-vous plut\u00f4t t\u00e9l\u00e9charger la liste au format csv ?",
+  "Vous n'avez pas la permission de t\u00e9l\u00e9charger au format csv" : "Vous n'avez pas la permission de t\u00e9l\u00e9charger au format csv",
+  "oui" : "oui"
 }
