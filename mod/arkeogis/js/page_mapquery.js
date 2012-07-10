dd070985ed723c23866b29e9b993de55fe74ce1f
Christophe Beveraggi <beve@croll.fr>
2012-07-10 10:24:07 +0200

    - Mapquery JS debug - New CSV for documentation

--- mod/arkeogis/js/page_mapquery.js
+++ mod/arkeogis/js/page_mapquery.js
@@ -431,17 +431,25 @@ function populateSavedQueriesMenu() {
 	    var sel=$('select-savedqueries');
 	    var id=sel.getSelected().get('value');
 	    sel.set('html', '');
-	    Array.push(sel.options, (new Element('option', {
+			sel.adopt(new Element('option', {
 		'value': '0',
 		'text': ch_t('arkeogis', "Requêtes archivées")
-	    })));
+	    }));
+	    /*Array.push(sel.options, (new Element('option', {
+		'value': '0',
+		'text': ch_t('arkeogis', "Requêtes archivées")
+	    })));*/
 	    var index=1;
 	    res.each(function(line) {
-		Array.push(sel.options, (new Element('option', {
+			sel.adopt(new Element('option', {
 		    'value': line.id,
 		    'text': line.name
-		})));
-		if (typeOf('line') != undefined && line.id == id) {
+		}));
+		/*Array.push(sel.options, (new Element('option', {
+		    'value': line.id,
+		    'text': line.name
+		})));*/
+		if (typeOf(line) != undefined && line.id == id) {
 			sel.selectedIndex=index;
 		}
 		index++;
