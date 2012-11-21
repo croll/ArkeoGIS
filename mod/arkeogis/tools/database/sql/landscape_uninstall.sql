36fbbd0bd87ba45482a9f75ea1aa1c3694ff5de7
Nicolas Dimitrijevic <nicolas@croll.fr>
2012-11-21 22:50:22 +0100

    landscape sql generated now ok

--- mod/arkeogis/tools/database/sql/landscape_uninstall.sql
+++ mod/arkeogis/tools/database/sql/landscape_uninstall.sql
@@ -1,6 +1,6 @@
-DROP INDEX IF EXISTS "idx_landscape_node_path_btree_idx";
-DROP INDEX IF EXISTS "idx_landscape_node_path_gist_idx";
-DROP FUNCTION IF EXISTS get_calculated_ark__node_path(integer);
-DROP TRIGGER trig01_update_ark__node_path ON landscape;
-DROP FUNCTION IF EXISTS trig_update_ark__node_path();
-DROP TABLE IF EXISTS "landscape";
+DROP INDEX IF EXISTS "idx_ark_landscape_node_path_btree_idx";
+DROP INDEX IF EXISTS "idx_ark_landscape_node_path_gist_idx";
+DROP FUNCTION IF EXISTS get_calculated_la_node_path(integer);
+DROP TRIGGER trig01_update_la_node_path ON ark_landscape;
+DROP FUNCTION IF EXISTS trig_update_la_node_path();
+DROP TABLE IF EXISTS "ark_landscape";
